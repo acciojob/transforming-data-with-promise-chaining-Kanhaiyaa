4e1b2ab0d05d2904f464771b78d49ace27b58810
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-01-23 17:11:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //your JS code here. If required.
-//your JS code here. If required.
 const input = document.getElementById('ip');
 const output = document.getElementById('output');
 const btn = document.getElementById('btn');
